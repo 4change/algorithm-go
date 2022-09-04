0630e8e95b7dd16086fa9e896ab75573842c03f7
4change <e_mail4gxf@163.com>
2022-09-04 19:41:33 +0800

    0028.move-zeroes

---
+++ leetcode/0028.move-zeroes/move-zeroes.go
@@ -0,0 +1,19 @@
+package problem0028
+
+func moveZeroes(nums []int)  {
+    cnt := 0
+
+    // 过滤器思想——过滤大于0的元素到数组的前半部分
+    for _, num := range nums {
+        if (num != 0) {
+            nums[cnt] = num
+            cnt++
+        }
+    }
+
+    // 数组的后半部分补0
+    for cnt < len(nums) {
+        nums[cnt] = 0
+        cnt++
+    }
+}
