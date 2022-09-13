12ad3452bdc3bbccb05a9e345f00a8326dbe1d9e
4change <e_mail4gxf@163.com>
2022-09-13 17:41:46 +0800

    0088.merge-sorted-array

--- leetcode/0088.merge-sorted-array/merge-sorted-array.go
+++ leetcode/0088.merge-sorted-array/merge-sorted-array.go
@@ -1,12 +1,12 @@
 package problem0088
 
 func merge_1(nums1 []int, m int, nums2 []int, n int)  {
-    var res []int
+    var res []int       // 新数组用于存储比较结果
     var i = 0
     var j = 0
 
     for (i < m || j < n) {
-        if (j >= n || (i < m && nums1[i] <= nums2[j])) {
+        if (j >= n || (i < m && nums1[i] <= nums2[j])) {    // 注意边界条件的判定
             res = append(res, nums1[i])
             i++
         } else {
@@ -23,7 +23,7 @@ func merge_1(nums1 []int, m int, nums2 []int, n int)  {
 func merge_2(nums1 []int, m int, nums2 []int, n int)  {
     var i = m - 1
     var j = n - 1
-    var cnt = m + n -1
+    var cnt = m + n -1      // nums1 数组的最后一个元素的下标
 
     for i >= 0 || j >= 0 {
         if j < 0 || (i >= 0 && nums1[i] > nums2[j]) {
