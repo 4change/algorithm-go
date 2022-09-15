a0f18845aad9fee1c561b95306e1e31394396bce
4change <e_mail4gxf@163.com>
2022-09-15 10:44:34 +0800

    0088.merge-sorted-array

--- leetcode/0088.merge-sorted-array/merge-sorted-array.go
+++ leetcode/0088.merge-sorted-array/merge-sorted-array.go
@@ -1,5 +1,8 @@
 package problem0088
 
+// 解法1：从前往后遍历，使用到临时数组 res 存放中间结果
+// 时间复杂度：O(m+n)
+// 空间复杂度：O(m+n)
 func merge_1(nums1 []int, m int, nums2 []int, n int)  {
     var res []int       // 新数组用于存储比较结果
     var i = 0
@@ -20,13 +23,18 @@ func merge_1(nums1 []int, m int, nums2 []int, n int)  {
     }
 }
 
+// 解法二：从后往前遍历，只使用 nums1 作为存放最终结果的数组
+// 时间复杂度: O(m+n)
+// 空间复杂度：O(1)
 func merge_2(nums1 []int, m int, nums2 []int, n int)  {
     var i = m - 1
     var j = n - 1           // nums2 数组的最后一个元素的下标
     var cnt = m + n -1      // nums1 数组的最后一个元素的下标
 
     for i >= 0 || j >= 0 {
-        if j < 0 || (i >= 0 && nums1[i] > nums2[j]) {   // 注意边界条件的判定
+        // 注意边界条件的判定
+        // j < 0 || (i >= 0 && nums1[i] > nums2[j])    表示 "num2 已遍历完" or "num1 和 num2 未遍历完，并且 num1[i] > num2[j]"
+        if j < 0 || (i >= 0 && nums1[i] > nums2[j]) {   
             nums1[cnt] = nums1[i]
             i--
         } else {
