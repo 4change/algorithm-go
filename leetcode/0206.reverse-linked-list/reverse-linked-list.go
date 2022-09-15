08aef262485edbe2aa1bd2e33422be02b0426fed
4change <e_mail4gxf@163.com>
2022-09-15 10:04:26 +0800

    0206.reverse-linked-list

--- leetcode/0206.reverse-linked-list/reverse-linked-list.go
+++ leetcode/0206.reverse-linked-list/reverse-linked-list.go
@@ -1,13 +1,15 @@
 package problem0206
 
 func reverseList(head *ListNode) *ListNode {
-	var help *ListNode
+	var help *ListNode			// 新链表的头结点
 
 	for head != nil {
-		next := head.Next
-		head.Next = help
-		help = head
-		head = next
+		next := head.Next		// 确定右边子链表的下一节点位置
+
+		head.Next = help		// 断开
+
+		help = head             // 确定左边子链表当前节点的位置
+		head = next				// 确定右边子链表当前节点的位置
 	}
 	
 	return help
